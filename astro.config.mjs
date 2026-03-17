72b7f76dbef529372aa2bc7c3857cfceb6d31959
Francisco  Zamorano Vera <franciscozamoranovera@Franciscos-MacBook-Pro-2.local>
2026-03-17 14:20:36 -0300

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 
 export default defineConfig({
   output: 'server', //enable SSR
+  adapter: vercel(),
   build: {
       server: './server',
       assets: 'assets',
